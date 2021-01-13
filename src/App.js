4a78eab60c169070e8acaa7bbb57012883e9b95e
Melissa Preece <melissanchez@gmail.com>
2021-01-13 12:45:09 -0800

    State and fetch issues resolved

--- src/App.js
+++ src/App.js
@@ -34,9 +34,8 @@ class App extends Component {
   componentDidMount() {
     fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${photoApiKey}&tags=beaches%2C+deserts%2C+mountains&per_page=24&page=1&format=json&nojsoncallback=1`)
       .then(response => response.json())
-      .then ( responseData => {
-        // this.setState({ photos: responseData.data });
-        this.setState(state => ({...state, photos: state.photos})); //taken from https://ozmoroz.com/2018/11/why-my-setstate-doesnt-work/
+      .then (responseData => {
+        this.setState({ photos: responseData }); //works now, but in Guil's demo, he uses responseData.data as the new state, don't understand...
         console.log(this.state.photos);
       })
       .catch(error => {
